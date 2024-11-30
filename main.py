b28386854e3af52f66b5353525753666027529d8
TreasureCoded-locked-in <akpannsikak410@gmail.com>
2024-11-30 16:45:56 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+var = 10        #initialise
+while var > 0:   #iterate loop
+   var = var-1
+   if var == 3:    #condition 1
+      continue
+   print ('\nCurrent variable value: ',var)
+print ('\nGoodbye!')
